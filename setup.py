0e510acf54c290f91f20e1b71d9ae97e169387f7
Sascha Peilicke <saschpe@gmx.de>
2010-12-06 22:44:45 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+f = open('README')
+try:
+    README = f.read()
+finally:
+    f.close()
+
+setup(name='egg2rpm', version='0.1', license="GPLv2",
+      description='Script to convert a Python egg to a RPM spec file',
+      long_description=README,
+      author='Sascha Peilicke', author_email='saschpe@gmx.de',
+      url='http://github.com/saschpe/egg2rpm'.
+      packages=['egg2rpm'],
+      scripts=['egg2rpm/egg2rpm.py'],
+      #install_requires=[]
+)
