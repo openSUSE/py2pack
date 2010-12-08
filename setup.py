afa1622d5c32ece4c67e5de838dab9979f05346f
Sascha Peilicke <saschpe@gmx.de>
2010-12-08 21:27:28 +0100

    Some refactoring. Refer to 'module' instead of ambiguous 'package'.

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ try:
 finally:
     f.close()
 
-setup(name='egg2rpm', version='0.1', license="GPLv2",
-      description='Script to convert a Python egg to a RPM spec file',
-      long_description=README,
-      author='Sascha Peilicke', author_email='saschpe@gmx.de',
-      url='http://github.com/saschpe/egg2rpm',
-      scripts=['egg2rpm'],
-      install_requires=['argparse']
+setup(
+    name = 'egg2rpm',
+    version = '0.1',
+    license = "GPLv2",
+    description = 'Generate packages from Python modules on PyPI'
+    long_description = README,
+    author = 'Sascha Peilicke',
+    author_email = 'saschpe@gmx.de',
+    url = 'http://github.com/saschpe/egg2rpm',
+    scripts = ['egg2rpm'],
+    install_requires = ['argparse']
 )
