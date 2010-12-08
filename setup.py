9a23d2a4e9cdcb81e4f20089a807488c11ba4a6c
Sascha Peilicke <saschpe@gmx.de>
2010-12-08 21:35:20 +0100

    Renamed to 'py2pack', some minor improvements along the way.

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ finally:
     f.close()
 
 setup(
-    name = 'egg2rpm',
+    name = 'py2pack',
     version = '0.1',
     license = "GPLv2",
     description = 'Generate packages from Python modules on PyPI'
     long_description = README,
     author = 'Sascha Peilicke',
     author_email = 'saschpe@gmx.de',
-    url = 'http://github.com/saschpe/egg2rpm',
-    scripts = ['egg2rpm'],
+    url = 'http://github.com/saschpe/py2pack',
+    scripts = ['py2pack'],
     install_requires = ['argparse']
 )
