bab6221e475fd1797c0a281d5d7c9e4e1cc924e3
Sascha Peilicke <saschpe@suse.de>
2010-12-09 11:50:54 +0100

    The 'package'->'module' rename broke quite some things, renaming back.

--- py2pack/py2pack.py
+++ py2pack/py2pack.py
@@ -28,28 +28,28 @@ pypi = xmlrpclib.ServerProxy('http://python.org/pypi')              # XML RPC co
 env = Environment(loader = FileSystemLoader(TEMPLATE_DIR))          # Jinja2 template environment
 
 def list(args):
-    print('listing all PyPI modules...')
-    for module in pypi.list_modules():
-        print(module)
+    print('listing all PyPI packages...')
+    for package in pypi.list_packages():
+        print(package)
 
 def search(args):
-    print('searching for module {0}...'.format(args.name))
+    print('searching for package {0}...'.format(args.name))
     for hit in pypi.search({'name': args.name}):
         print('found {0}-{1}'.format(hit['name'], hit['version']))
 
 def show(args):
     if not args.version:                                            # take first version found
         args.version = pypi.search({'name': args.name})[0]['version']
-    print('showing module {0}...'.format(args.name))
+    print('showing package {0}...'.format(args.name))
     data = pypi.release_data(args.name, args.version)               # fetch all meta data
     pprint(data)
 
 def fetch(args):
     if not args.version:                                            # take first version found
         args.version = pypi.search({'name': args.name})[0]['version']
-    print('downloading module {0}-{1}...'.format(args.name, args.version))
-    for url in pypi.module_urls(args.name, args.version):          # fetch all download URLs
-        if url['moduletype'] == 'sdist':                           # found the source URL we care for
+    print('downloading package {0}-{1}...'.format(args.name, args.version))
+    for url in pypi.package_urls(args.name, args.version):          # fetch all download URLs
+        if url['packagetype'] == 'sdist':                           # found the source URL we care for
             print('from {0}'.format(url['url']))
             urllib.urlretrieve(url['url'], url['filename'])         # download the object behind the URL
 
@@ -70,35 +70,35 @@ def gen_dsc(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'Generate packages from Python modules on PyPI')
+    parser = argparse.ArgumentParser(description = 'Generate packages from Python packages on PyPI')
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
     subparsers = parser.add_subparsers(title='commands')
 
-    parser_list = subparsers.add_parser('list', help='list all modules on PyPI')
+    parser_list = subparsers.add_parser('list', help='list all packages on PyPI')
     parser_list.set_defaults(func=list)
 
-    parser_search = subparsers.add_parser('search', help='search for modules on PyPI')
-    parser_search.add_argument('name', help='module name')
+    parser_search = subparsers.add_parser('search', help='search for packages on PyPI')
+    parser_search.add_argument('name', help='package name')
     parser_search.set_defaults(func=search)
 
-    parser_show = subparsers.add_parser('show', help='show metadata for module')
-    parser_show.add_argument('name', help='module name')
-    parser_show.add_argument('version', nargs='?', help='module version (optional)')
+    parser_show = subparsers.add_parser('show', help='show metadata for package')
+    parser_show.add_argument('name', help='package name')
+    parser_show.add_argument('version', nargs='?', help='package version (optional)')
     parser_show.set_defaults(func=show)
 
-    parser_fetch = subparsers.add_parser('fetch', help='download module source tarball from PyPI')
-    parser_fetch.add_argument('name', help='module name')
-    parser_fetch.add_argument('version', nargs='?', help='module version (optional)')
+    parser_fetch = subparsers.add_parser('fetch', help='download package source tarball from PyPI')
+    parser_fetch.add_argument('name', help='package name')
+    parser_fetch.add_argument('version', nargs='?', help='package version (optional)')
     parser_fetch.set_defaults(func=fetch)
 
-    parser_spec = subparsers.add_parser('genspec', help='generate RPM spec file for a module')
-    parser_spec.add_argument('name', help='module name')
-    parser_spec.add_argument('version', nargs='?', help='module version (optional)')
+    parser_spec = subparsers.add_parser('genspec', help='generate RPM spec file for a package')
+    parser_spec.add_argument('name', help='package name')
+    parser_spec.add_argument('version', nargs='?', help='package version (optional)')
     parser_spec.add_argument('template', choices=['opensuse'], help='spec file template')
     parser_spec.set_defaults(func=gen_spec)
 
     parser_help = subparsers.add_parser('help', help='show this help')
-    # TODO: allow to display module-specific help too
+    # TODO: allow to display package-specific help too
     parser_help.set_defaults(func=lambda args: parser.print_help())
 
     args = parser.parse_args()
