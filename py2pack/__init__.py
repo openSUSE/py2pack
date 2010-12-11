889de28ca2b35df1245db3d8ea6e4f49ee3f28c2
Sascha Peilicke <saschpe@gmx.de>
2010-12-11 17:11:43 +0100

    Import interesting functions from submodule (simplifies API).

--- py2pack/__init__.py
+++ py2pack/__init__.py
@@ -1,3 +1,5 @@
 __doc__ = 'Generate distribution packages from Python packages on PyPI'
 __author__ = 'Sascha Peilicke <saschpe@gmx.de>'
-__version__ = '0.2.3'
+__version__ = '0.2.4'
+
+from py2pack import list, search, fetch, generate, main
