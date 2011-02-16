d55e549ff4eaa68a12c149cfdce943ae9b432dd7
Sascha Peilicke <saschpe@suse.de>
2011-02-16 09:39:03 +0100

    Bump version number.

--- py2pack/__init__.py
+++ py2pack/__init__.py
@@ -1,5 +1,5 @@
 __doc__ = 'Generate distribution packages from Python packages on PyPI'
 __author__ = 'Sascha Peilicke <saschpe@gmx.de>'
-__version__ = '0.3.10'
+__version__ = '0.3.11'
 
 from py2pack import list, search, fetch, generate, main
