64482af5996918b0ce425e7e11a5fb5df72799a5
Sascha Peilicke <saschpe@suse.de>
2011-09-22 11:59:54 +0200

    Bump version to 0.3.21

--- py2pack/__init__.py
+++ py2pack/__init__.py
@@ -20,6 +20,6 @@
 __doc__ = 'Generate distribution packages from Python packages on PyPI'
 __docformat__ = 'restructuredtext en'
 __author__ = 'Sascha Peilicke <saschpe@gmx.de>'
-__version__ = '0.3.20'
+__version__ = '0.3.21'
 
 from py2pack import list, search, fetch, generate, main
