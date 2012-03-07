a537d5a2c74e36408546730a5ab878f7244a09c3
Sascha Peilicke <saschpe@suse.de>
2012-03-07 09:54:16 +0100

    Rename dir 'doc' to 'docs'

--- setup.py
+++ setup.py
@@ -33,19 +33,19 @@ if sys.argv[-1] == "doc":
     """Generate manpage, HTML and PDF documentation.
     """
     try:
-        subprocess.call(["xsltproc", "--output", "doc/py2pack.html", "/usr/share/xml/docbook/stylesheet/nwalsh/current/html/docbook.xsl", "doc/src/py2pack.xml.in"])
-        subprocess.call(["xsltproc", "--output", "doc/py2pack.1", "/usr/share/xml/docbook/stylesheet/nwalsh/current/manpages/docbook.xsl", "doc/src/py2pack.xml.in"])
+        subprocess.call(["xsltproc", "--output", "docs/py2pack.html", "/usr/share/xml/docbook/stylesheet/nwalsh/current/html/docbook.xsl", "docs/src/py2pack.xml.in"])
+        subprocess.call(["xsltproc", "--output", "docs/py2pack.1", "/usr/share/xml/docbook/stylesheet/nwalsh/current/manpages/docbook.xsl", "docs/src/py2pack.xml.in"])
         #subprocess.call(["xsltproc", "--output", "doc/py2pack.fo",
         #                 "--stringparam", "paper.type", "A4",
         #                 "--stringparam", "body.start.indent", "0pt",
         #                 "--stringparam", "title.margin.left", "0pt",
         #                 "--stringparam", "variablelist.as.blocks", "1",
-        #                 "/usr/share/xml/docbook/stylesheet/nwalsh/current/fo/docbook.xsl", "doc/py2pack.xml.in"])
-        #subprocess.call(["fop", "doc/py2pack.fo", "doc/py2pack.pdf"])
+        #                 "/usr/share/xml/docbook/stylesheet/nwalsh/current/fo/docbook.xsl", "docs/py2pack.xml.in"])
+        #subprocess.call(["fop", "docs/py2pack.fo", "docs/py2pack.pdf"])
     except:
         pass
-    #if os.path.exists("doc/py2pack.fo"):
-    #    os.remove("doc/py2pack.fo")
+    #if os.path.exists("docs/py2pack.fo"):
+    #    os.remove("docs/py2pack.fo")
     sys.exit()
 
 setup(
@@ -61,8 +61,8 @@ setup(
     packages=['py2pack'],
     package_data={'py2pack': ['templates/*']},
     data_files=[('share/doc/py2pack', ['AUTHORS', 'LICENSE', 'README.rst']),
-                ('share/doc/py2pack/html', ['doc/py2pack.html']),
-               #('share/doc/py2pack/pdf', ['doc/py2pack.pdf']),
+                ('share/doc/py2pack/html', ['docs/py2pack.html']),
+               #('share/doc/py2pack/pdf', ['docs/py2pack.pdf']),
                 ('man/man1', ['doc/py2pack.1'])],
     requires=['argparse', 'Jinja2'],
     classifiers=[
