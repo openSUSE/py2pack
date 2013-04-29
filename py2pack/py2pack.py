f7db643a3d77c011dd39e4a8c954884bd64e0dbf
Sascha Peilicke <saschpe@suse.de>
2013-04-29 15:11:16 +0200

    Release 0.4.3.1
    Bugfix release, forget pickled SDPX license map file.

--- py2pack/py2pack.py
+++ py2pack/py2pack.py
@@ -37,7 +37,7 @@ from pprint import pprint
 warnings.filterwarnings('ignore', 'Module argparse was already imported')   # Filter a UserWarning from Jinja2
 import jinja2
 
-__version__ = '0.4.3'
+__version__ = '0.4.3.1'
 
 
 class ProxiedTransport(xmlrpclib.Transport):
