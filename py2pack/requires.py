a02899382ecd2ed214f8eef0078b253763cd7b39
Adam Spiers <py2pack@adamspiers.org>
2016-06-08 13:12:53 +0100

    protect against failed eval of setup.py code fragments
    When we're parsing setup.py instead of running it, we extract code
    fragments, and then eval them in the hope of obtaining the correct
    value.  However this is highly prone to failure, so at least attempt to
    fail gracefully in a way which guides the user towards the right manual
    fix.
    
    This does not fix https://github.com/saschpe/py2pack/issues/41 but
    at least it makes it clearer what the issue is.

--- py2pack/requires.py
+++ py2pack/requires.py
@@ -23,37 +23,69 @@ import os
 import re
 import six
 import subprocess
+import sys
 import tempfile
+import textwrap
+
+
+def _safe_eval(descr, code, fallback):
+    try:
+        return eval(code)
+    except:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        code = re.sub("(?m)^", "  ", code)
+        sys.stderr.write(
+            textwrap.dedent("""\
+
+            WARNING: Exception encountered:
+
+              %s: %s
+
+            whilst eval'ing code for '%s' parameter in setup.py:
+
+            %s
+
+            Try regenerating with the --run option.
+            """) % (exc_type.__name__, exc_value, descr, code))
+        return fallback
 
 
 def _requires_from_setup_py(file):
     """read requirements from the setup.py file"""
     data = {}
     contents = six.u(file.read())
+    fixme = "FIXME: failed to extract %s from setup.py"
     match = re.search("ext_modules", contents)
     if match:
         data["is_extension"] = True
     match = re.search("[(,]\s*scripts\s*=\s*(\[.*?\])", contents, flags=re.DOTALL)
     if match:
-        data["scripts"] = eval(match.group(1))
+        data["scripts"] = \
+            _safe_eval("scripts", match.group(1), [fixme % "scripts"])
     match = re.search("test_suite\s*=\s*(.*)", contents)
     if match:
-        data["test_suite"] = eval(match.group(1))
+        data["test_suite"] = \
+            _safe_eval("test_suite", match.group(1), [fixme % "test_suite"])
     match = re.search("install_requires\s*=\s*(\[.*?\])", contents, flags=re.DOTALL)
     if match:
-        data["install_requires"] = eval(match.group(1))
+        data["install_requires"] = \
+            _safe_eval("install_requires", match.group(1), [fixme % "install_requires"])
     match = re.search("extras_require\s*=\s*(\{.*?\})", contents, flags=re.DOTALL)
     if match:
-        data["extras_require"] = eval(match.group(1))
+        data["extras_require"] = \
+            _safe_eval("extras_require", match.group(1), [fixme % "extras_require"])
     match = re.search("tests_require\s*=\s*(\{.*?\})", contents, flags=re.DOTALL)
     if match:
-        data["tests_require"] = eval(match.group(1))
+        data["tests_require"] = \
+            _safe_eval("tests_require", match.group(1), [fixme % "tests_require"])
     match = re.search("data_files\s*=\s*(\[.*?\])", contents, flags=re.DOTALL)
     if match:
-        data["data_files"] = eval(match.group(1))
+        data["data_files"] = \
+            _safe_eval("data_files", match.group(1), [fixme % "data_files"])
     match = re.search('entry_points\s*=\s*(\{.*?\}|""".*?"""|".*?")', contents, flags=re.DOTALL)
     if match:
-        data["entry_points"] = eval(match.group(1))
+        data["entry_points"] = \
+            _safe_eval("entry_points", match.group(1), [fixme % "entry_points"])
     return data
 
 
