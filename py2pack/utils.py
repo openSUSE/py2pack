b1d1c281464dd33281ded4f6809a5159a66f12dd
Dan Čermák <dcermak@suse.com>
2018-12-14 15:48:19 +0100

    Add documentation to py2pack/utils.py

--- py2pack/utils.py
+++ py2pack/utils.py
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Module containing utility functions that fit nowhere else."""
+
 from typing import List  # noqa: F401, pylint: disable=unused-import
 
 import tarfile
@@ -23,6 +25,19 @@ import zipfile
 
 def _get_archive_filelist(filename):
     # type: (str) -> List[str]
+    """Extract the list of files from a tar or zip archive.
+
+    Args:
+        filename: name of the archive
+
+    Returns:
+        Sorted list of files in the archive, excluding './'
+
+    Raises:
+        ValueError: when the file is neither a zip nor a tar archive
+        FileNotFoundError: when the provided file does not exist (for Python 3)
+        IOError: when the provided file does not exist (for Python 2)
+    """
     names = []  # type: List[str]
     if tarfile.is_tarfile(filename):
         with tarfile.open(filename) as tar_file:
